e2e454b3efff772d3f35774a085c08102c4ac106
Diwash Mainali <diwashmainali900@gmail.com>
2023-03-13 18:01:01 +0545

    Supplier CRUD completed

--- IVM/resources/views/backend/supplier/supplier_edit.blade.php
+++ IVM/resources/views/backend/supplier/supplier_edit.blade.php
@@ -1,61 +1,75 @@
 @extends('admin.admin_master')
 @section('admin')
-
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
     <div class="page-content">
         <div class="container-fluid">
+
             <div class="row">
                 <div class="col-12">
                     <div class="card">
                         <div class="card-body">
 
-                            <h4 class="card-title">Add Supplier</h4><br><br>
+                            <h4 class="card-title">Add Supplier Page </h4><br><br>
 
-                            <form method="post" action="{{ route('supplier.store') }}" id="myForm">
+
+
+                            <form method="post" action="{{ route('supplier.update') }}" id="myForm" >
+{{--                                <form method="post" id="myForm" >--}}
+
                                 @csrf
+
+                                <input type="hidden" name="id" value="{{ $supplier->id }}">
+
                                 <div class="row mb-3">
-                                    <label for="example-text-input" class="col-sm-2 col-form-label">Supplier Name</label>
+                                    <label for="example-text-input" class="col-sm-2 col-form-label">Supplier Name </label>
                                     <div class="form-group col-sm-10">
-                                        <input name="name" class="form-control" type="text">
+                                        <input name="name" class="form-control" value="{{ $supplier->name }}" type="text"    >
                                     </div>
                                 </div>
                                 <!-- end row -->
 
+
                                 <div class="row mb-3">
-                                    <label for="example-text-input" class="col-sm-2 col-form-label">Supplier Mobile No.</label>
+                                    <label for="example-text-input" class="col-sm-2 col-form-label">Supplier Mobile </label>
                                     <div class="form-group col-sm-10">
-                                        <input name="mobile_no" class="form-control" type="text">
+                                        <input name="mobile_no" value="{{ $supplier->mobile_no }}" class="form-control" type="text"    >
                                     </div>
                                 </div>
                                 <!-- end row -->
 
+
                                 <div class="row mb-3">
-                                    <label for="example-text-input" class="col-sm-2 col-form-label">Supplier Email</label>
+                                    <label for="example-text-input" class="col-sm-2 col-form-label">Supplier Email </label>
                                     <div class="form-group col-sm-10">
-                                        <input name="email" class="form-control" type="email">
+                                        <input name="email" value="{{ $supplier->email }}" class="form-control" type="email"  >
                                     </div>
                                 </div>
                                 <!-- end row -->
 
+
                                 <div class="row mb-3">
-                                    <label for="example-text-input" class="col-sm-2 col-form-label">Supplier Address</label>
+                                    <label for="example-text-input" class="col-sm-2 col-form-label">Supplier Address </label>
                                     <div class="form-group col-sm-10">
-                                        <input name="address" class="form-control" type="text">
+                                        <input name="address" value="{{ $supplier->address }}" class="form-control" type="text"  >
                                     </div>
                                 </div>
                                 <!-- end row -->
-                                <center>
-                                    <input type="submit" class="btn btn-dark waves-effect waves-light" value="Add Supplier">
-                                </center>
+
+                                <input type="submit" class="btn btn-dark btn-rounded waves-effect waves-light" value="Update Supplier">
                             </form>
+
+
+
                         </div>
                     </div>
                 </div> <!-- end col -->
             </div>
+
+
+
         </div>
     </div>
-
 
     <script type="text/javascript">
         $(document).ready(function (){
@@ -103,4 +117,7 @@
         });
 
     </script>
+
+
+
 @endsection
