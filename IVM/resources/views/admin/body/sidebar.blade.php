859f2091a48aef7d79a1fb0e5b1c029de8e5a2f8
adhiraj1212 <np03cs4s220190@heraldcollege.edu.np>
2023-05-04 10:30:37 +0545

    customer-wise report credit/paid completed

--- IVM/resources/views/admin/body/sidebar.blade.php
+++ IVM/resources/views/admin/body/sidebar.blade.php
@@ -36,7 +36,9 @@
                     <ul class="sub-menu" aria-expanded="false">
                         <li><a href="{{ route('customer.all') }}">All Customers</a></li>
                         <li><a href="{{ route('credit.customer') }}">Credit Customers</a></li>
+
                         <li><a href="{{ route('paid.customer') }}">Paid Customers</a></li>
+                        <li><a href="{{ route('customer.wise.report') }}">Customer Wise Report</a></li>
                     </ul>
                 </li>
 
